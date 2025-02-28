dead5f5498e08b56215719ac449a853d81ff63df
Artem Repko <a.repko33@gmail.com>
2025-02-28 20:58:50 +0200

    Fix

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { getEnvVar } from './utils/getEnvVar.js';
 import contactsRouter from './routes/contacts-routers.js';
+import authRouter from './routes/auth.js';
 import { logger } from './middlewares/logger.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
@@ -10,8 +12,13 @@ export const startServer = () => {
   const app = express();
   app.use(cors());
   app.use(express.json());
+  app.use(cookieParser());
+
   app.use(logger);
+
+  app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
+
   app.use(notFoundHandler);
   app.use(errorHandler);
 
